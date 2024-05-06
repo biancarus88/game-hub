24c79f0ebfa4775631464e26b90c3d14135f6c21
INTERN\bianca.hila <rus.bianca88@gmail.com>
2024-05-06 22:14:20 +0300

    added useGames custom hook

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface GetGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+function useGames() {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    apiClient
+      .get<GetGamesResponse>("/games", {
+        signal: controller.signal,
+      })
+      .then((response) => setGames(response.data.results))
+      .catch((error) => {
+        if (error instanceof CanceledError) return;
+        setError(error.message);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { games, error };
+}
+
+export default useGames;
