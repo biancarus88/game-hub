228c00adc97074abd7a8341b64bccd97ac4a5a65
INTERN\bianca.hila <rus.bianca88@gmail.com>
2024-05-07 11:54:03 +0300

    added GenreList component

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,15 @@
+import useGenres from "../hooks/useGenres";
+
+function GenreList() {
+  const { genres, error, isLoading } = useGenres();
+
+  return (
+    <ul>
+      {genres.map((g) => (
+        <li key={g.id}>{g.name}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default GenreList;
---
+++ src/hooks/useGenres.ts
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface Genre {
+  id: number;
+  name: string;
+}
+
+interface GetGenresResponse {
+  count: number;
+  results: Genre[];
+}
+
+function useGenres() {
+  const [genres, setGenres] = useState<Genre[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    setLoading(true);
+
+    apiClient
+      .get<GetGenresResponse>("/genres", {
+        signal: controller.signal,
+      })
+      .then((response) => setGenres(response.data.results))
+      .catch((error) => {
+        if (error instanceof CanceledError) return;
+        setError(error.message);
+      })
+      .finally(() => setLoading(false));
+
+    return () => controller.abort();
+  }, []);
+
+  return { genres, error, isLoading };
+}
+
+export default useGenres;
